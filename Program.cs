d7d75c12226ffee408957b2488a8446f50603776
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-01-25 15:51:58 -0300

    finishing texteditor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TextEditor
 {
@@ -23,11 +24,61 @@ namespace TextEditor
             {
                 case 0: System.Environment.Exit(0); break;
                 case 1: Open(); break;
-                case 2: New(); break;
+                case 2: Edit(); break;
             }
         }
 
-        static void Open() { }
-        static void New() { }
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadLine();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+        static void Edit()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite seu texto abaixo (Aperte ESC para sair)");
+            Console.WriteLine("---------------------");
+
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+        }
     }
 }
